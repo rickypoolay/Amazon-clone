1874063f1818a68e9d6d15765fbf8ca42af69a30
Ricardo Poullet <rickypoullet@gmail.com>
2022-01-21 14:40:17 -0500

    finalized

---
+++ src/requests.js
@@ -0,0 +1,14 @@
+const API_KEY = "eecb7076ed651b1a4022b1f22fb5fd93";
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentariesMovies: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+
+export default requests;
